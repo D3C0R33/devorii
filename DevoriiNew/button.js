d590d1bec363ca95afa8886916f4f516916f9f38
Devorii <you@example.com>
2018-07-08 16:48:16 -0400

    Master edit file of Devorii.com

---
+++ DevoriiNew/button.js
@@ -0,0 +1,21 @@
+function graphics(){ 
+    window.open('gfx.html', '_parent');
+}
+
+function contactUs(){ 
+    window.open('contact.html', '_parent');
+}
+
+function webProjects(){ 
+    window.open('web-projects.html', '_parent');
+}
+
+function UIUXProjects(){ 
+    window.open('uiux.html', '_parent');
+}
+
+function fourOFour(){
+    window.open('404.html', '_parent');
+}
+
+   
---
+++ DevoriiNew/main.js
@@ -0,0 +1,16 @@
+function myFunction(x) {
+    x.classList.toggle("change"); 
+
+     var x = document.getElementById("menuContent");
+    
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
+
+   
